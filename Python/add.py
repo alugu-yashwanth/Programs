fcc2c64a6444e3a96f35cce2fff1622ae5d00831
alugu-yashwanth <alugu.yashwanth@gmail.com>
2023-10-17 11:05:51 +0530

    Escape Characters

--- Python/add.py
+++ Python/add.py
@@ -2,3 +2,12 @@
 # print statement is being used
 print(2+3)
 print(27+19)
+
+print ('My name is \"Yashwanth\"') 
+#Double Quote escape sequence character is \".....\"
+
+#seperator in print statement
+print("Hey", 98 , "b", sep="~")
+#end in print statement
+print("HEY ", end ="Yashwanth\n")
+print("Alugu")
