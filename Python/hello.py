f4315dfbcccf961d0d1a831547aa04e051ef543f
alugu-yashwanth <alugu.yashwanth@gmail.com>
2023-10-17 16:23:24 +0530

    More on Strings

--- Python/hello.py
+++ Python/hello.py
@@ -1,2 +1,12 @@
 print('Hello world!!')
 #my first programg
+
+str='''Triple quotations 
+are used 
+for multilined 
+commments'''
+print(str)
+
+#using for loop in string
+for i in str:
+    print(i)
