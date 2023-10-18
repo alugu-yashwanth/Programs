89e3a67ddb856551da70e66d1affeb3164a05858
alugu-yashwanth <alugu.yashwanth@gmail.com>
2023-10-18 22:15:26 +0530

    Renamed

--- Python/04 String Method.py
+++ Python/04 String Method.py
@@ -12,3 +12,10 @@ print(b.capitalize(),"\n")
 # converts first letter capital
 print(b.center(50),"\n")
 # adds pre space on to string to bring it to center
+print(b.count("e"),"\n")
+# count no of times the character has been occured
+print(b.endswith("vsdvsdvsvsvsvsfv"),"\n")
+# check for the given value end with gives in boolean
+print(b.endswith("ing",5,11))
+# check for value between indexes
+
