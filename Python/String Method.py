1b1667a3cec223479b50c6e4e807c5988355021d
alugu-yashwanth <alugu.yashwanth@gmail.com>
2023-10-18 21:01:06 +0530

    String Methods

---
+++ Python/String Method.py
@@ -0,0 +1,14 @@
+a="!!!!Hello!!!!!!!!!!!"
+print(len(a),"\n")
+print(a.upper(),"\n")
+print(a.lower(),"\n")
+print(a.rstrip("!"),"\n")
+# strip the trailing characteers in a string which are after the string
+print(a.replace("Hello", "Yashu"),"\n")
+b="i am trying different string methods"
+print(b.split(" "),"\n")
+# converts a string into list 
+print(b.capitalize(),"\n")
+# converts first letter capital
+print(b.center(50),"\n")
+# adds pre space on to string to bring it to center
