89273d228e12a2fcf3d3582c2cc54e6acf2fd925
alugu-yashwanth <alugu.yashwanth@gmail.com>
2023-10-19 14:55:09 +0530

    For Loops

---
+++ Python/07  for loops.py
@@ -0,0 +1,13 @@
+# x=input("Enter ny string: ")
+# for i in x:
+#     print(i, end=", ")
+
+# List=['Orange', 'Apple', 'Banana', 'Melon', 'Kiwi']
+# for fruit in List:
+#     print("\n",fruit)
+#     for i in fruit:
+#         print(i, end= ", ")
+
+for i in range(3,15,2):
+    print(i)
+    # here 3 is start 15 is end and 2 is difference between each iteration 
